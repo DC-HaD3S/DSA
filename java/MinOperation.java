e5299afc7eb49fce2fcdaa200266609072d87673
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-02-14 23:25:03 +0530

    fifth day

--- java/MinOperation.java
+++ java/MinOperation.java
@@ -1,19 +1,19 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 class MinOperations {
-    public int minOperations(int[] a, int k) {
-        Arrays.sort(a);
-        List<Integer> b = new ArrayList<>();
-        for (int i = 0, j = 0, count = 0, x, y; ; ++count) {
-            if (i < a.length && (j >= b.size() || a[i] <= b.get(j))) x = a[i++];
-            else x = b.get(j++);
-            
-            if (x >= k) return count;
-            if (i < a.length && (j >= b.size() || a[i] <= b.get(j))) y = a[i++];
-            else y = b.get(j++);
-            long t = 2L * x + y; 
-            b.add(t < k ? (int) t : k);
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.add((long) num);
         }
+        long cnt = 0;
+        while (pq.peek() < k) {
+            long first = pq.poll();
+            long second = pq.poll();
+            long num = 2 * Math.min(first, second) + Math.max(first, second);
+            pq.add(num);
+            cnt++;
+        }
+        return (int) cnt;
     }
 }
