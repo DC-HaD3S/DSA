c45035e2ee59d126a350e139327316290e21a98a
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-03-06 23:19:46 +0530

    25th day

--- java/InvertedRightPyramid.java
+++ java/InvertedRightPyramid.java
@@ -2,8 +2,8 @@ public class InvertedRightPyramid{
 public static void main(String[] args) {
 int n=5;
     for (int i = 0; i <n; i++) {
-        for (int j = n; j >i; j--) {
-            System.out.print(n-j+1);
+        for (int j = 1; j<n-i+1 ; j++) {
+            System.out.print(j);
         }
         System.out.println();
     }
