19cb4dbf6252f06431b42402bde9afd1c7b7533b
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-03-08 20:08:35 +0530

    27th day

--- java/MiniumumRecolors.java
+++ java/MiniumumRecolors.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 public class MiniumumRecolors{
     public int minimumRecolor(String blocks, int k) {
         int n = blocks.length();
@@ -19,5 +21,37 @@ public class MiniumumRecolors{
         }
         return minOperations;   
     }
+    //another optimized approach
+    class Solution {
+
+    public int minimumRecolors(String blocks, int k) {
+        Queue<Character> blockQueue = new LinkedList<>();
+        int numWhites = 0;
+
+        // Initiate queue with first k values
+        for (int i = 0; i < k; i++) {
+            char currentChar = blocks.charAt(i);
+            if (currentChar == 'W') numWhites++;
+            blockQueue.add(currentChar);
+        }
+
+        // Set initial minimum
+        int numRecolors = numWhites;
+
+        for (int i = k; i < blocks.length(); i++) {
+            // Remove front element from queue and update current number of white blocks
+            if (blockQueue.poll() == 'W') numWhites--;
+
+            // Add current element to queue and update current number of white blocks
+            char currentChar = blocks.charAt(i);
+            if (currentChar == 'W') numWhites++;
+            blockQueue.add(currentChar);
+
+            // Update minimum
+            numRecolors = Math.min(numRecolors, numWhites);
+        }
+        return numRecolors;
+    }
+}
 
 }
