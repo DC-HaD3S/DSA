a4d5bbd90922184abecae8b2b7121d9847a388db
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-03-29 21:37:53 +0530

    48th day

---
+++ java/ReverseDegreeString.java
@@ -0,0 +1,16 @@
+public class ReverseDegreeString{
+
+    public static int reverseDegree(String s) {
+        int sum = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            sum += (26 - (s.charAt(i) - 'a')) * (i + 1);
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(reverseDegree("abc"));  
+    }
+}
+
