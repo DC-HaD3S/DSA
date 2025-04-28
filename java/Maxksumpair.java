37c4ac5188866ef91c1a2f9648aa001e0d22f2ef
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-04-28 23:59:20 +0530

    77th day

---
+++ java/Maxksumpair.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Maxksumpair {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0;
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i] + nums[j];
+            if (temp == k) {
+                i++;
+                j--;
+                ans++;
+            } else if (temp > k) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
+}
