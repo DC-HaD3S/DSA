7fe9387d61f5914ce8d49156b5167f5c8cd1a6de
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-05-30 22:14:13 +0530

    88th day

--- java/ReverseDegreeString.java
+++ java/ReverseDegreeString.java
@@ -8,7 +8,6 @@ public class ReverseDegreeString{
         }
         return sum;
     }
-    
     public static void main(String[] args) {
         System.out.println(reverseDegree("abc"));  
     }
